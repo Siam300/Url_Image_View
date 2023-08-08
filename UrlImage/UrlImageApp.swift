5dc25349e68a9d050353d1ba9e9a054765b81a49
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-08-08 20:24:15 +0600

    Initial Commit

---
+++ UrlImage/UrlImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrlImageApp.swift
+//  UrlImage
+//
+//  Created by Auto on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct UrlImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
