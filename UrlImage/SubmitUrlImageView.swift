5bc851c667f8a9ac78f34d868cfc59ba71204f8d
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-08-09 21:03:03 +0600

    complete

--- UrlImage/SubmitUrlImageView.swift
+++ UrlImage/SubmitUrlImageView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct SubmitUrlImageView: View {
-    @State var imageUrl: String = ""
+    @State var imageUrl: String = "https://picsum.photos/id/237/200/300"
     @State var showImage: Bool = false
     //@State var imageUrl: String = ""
     var body: some View {
         
-        if imageUrl.isEmpty {
+        if !showImage {
             Text("Enter your Image Url Here!")
                 .font(.largeTitle)
                 .multilineTextAlignment(.center)
@@ -31,30 +31,28 @@ struct SubmitUrlImageView: View {
                     .background(Color.purple)
                     .cornerRadius(10)
             })
-        }
-        
-        else{
-            if imageUrl.count > 0 {
-                //let url = URL(string: imageUrl)
+        } else {
+            VStack {
                 AsyncImage(url: URL(string: imageUrl))
-                //.frame(maxWidth: .infinity, maxHeight: .infinity)
+                Spacer()
                 Button(action: {
-                    //delete()
+                    showImage.toggle()
                     imageUrl = ""
                 }, label: {
                     Text("Add another Url")
+                        .font(.title)
+                        .foregroundColor(Color.white)
+                        .padding(.horizontal)
+                        .background(Color.purple)
+                        .cornerRadius(10)
                 })
             }
+                
         }
     }
     func save(){
-        //       imageUrl.append(textFieldUrl)
-        //        showImage.toggle()
-        //imageUrl = textFieldUrl
-    }
-    func delete() {
-        imageUrl = ""
-        imageUrl = ""
+        showImage.toggle()
+        
     }
     
 }
